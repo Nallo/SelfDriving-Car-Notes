b8e2061df0e11db66d283a87883a44251b7ccfef
Stefano Martinallo <stefano@sherlock.bike>
2016-12-14 20:27:03 +0100

    [TensorFlow] add softmax example
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

---
+++ Lesson 08 TensorFlow/tensorflow_softmax.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+output = None
+logit_data = [2.0, 1.0, 0.1]
+logits = tf.placeholder(tf.float32)
+
+# TODO: Calculate the softmax of the logits
+softmax = tf.nn.softmax(logits)
+
+with tf.Session() as sess:
+    # TODO: Feed in the logit data
+    output = sess.run(softmax, feed_dict={logits: logit_data})
+
+print(output)
